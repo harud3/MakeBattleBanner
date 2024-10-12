91d15b852288b698d0e836bf47a5067f1cdd335a
harud3 <89560796+harud3@users.noreply.github.com>
2024-10-12 14:44:00 +0900

    first

---
+++ MakeBattleBanner/MBB.js
@@ -0,0 +1,15 @@
+function addTextToImage(){
+    var text = document.getElementById('userInput').value;
+    var img = document.getElementById('image');
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+    ctx.font = '30px Arial';
+    ctx.fillStyle = 'black';
+    ctx.fillText(text, 50, 50);
+    var dataUrl = canvas.toDataURL('image/png');
+    img.src = dataUrl;
+}
