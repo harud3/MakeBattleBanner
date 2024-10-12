6bbb1300c370a8463b7c4da3849f6a9d97dfc168
harud3 <89560796+harud3@users.noreply.github.com>
2024-10-12 15:37:08 +0900

    Add files via upload

--- MBB.js
+++ MBB.js
@@ -5,13 +5,14 @@ function addTextToImage(){
     var ctx = canvas.getContext('2d');
     canvas.width = img.width;
     canvas.height = img.height;
-
+    ctx.clearRect(0, 0, 210, 100);
     ctx.drawImage(img, 0, 0, img.width, img.height);
     ctx.font = 'bold 30px Arial';
     ctx.fillStyle = 'black';
     ctx.fillText(text, 210 - (text.length * 30), 95);
     var dataUrl = canvas.toDataURL('image/png');
     img.src = dataUrl;
+    ctx.clearRect(0, 0, 210, 100);
 
 
     img = document.getElementById('image2');
@@ -26,6 +27,7 @@ function addTextToImage(){
     ctx.fillText(text, 210 - (text.length * 30), 95);
     var dataUrl = canvas.toDataURL('image/png');
     img.src = dataUrl;
+    ctx.clearRect(0, 0, 210, 100);
 
 
     img = document.getElementById('image3');
@@ -34,10 +36,12 @@ function addTextToImage(){
     canvas.width = img.width;
     canvas.height = img.height;
 
+    ctx.clearRect(0, 0, 210, 100);
     ctx.drawImage(img, 0, 0, img.width, img.height);
     ctx.font = 'bold 30px Arial';
     ctx.fillStyle = 'black';
     ctx.fillText(text, 210 - (text.length * 30), 95);
     var dataUrl = canvas.toDataURL('image/png');
     img.src = dataUrl;
+    ctx.clearRect(0, 0, 210, 100);
 }
